03c9e1beaa9d2ff4fa334aa75508201715b14b05
Wibiati Sekar K <89697985+wsekar@users.noreply.github.com>
2021-11-16 19:46:09 +0700

    Add files via upload

---
+++ PGD11_TI E_V3920061_Wibiati Sekar Kinasih/pertemuan11.py
@@ -0,0 +1,129 @@
+# menghitung pergerakan kamera
+from math import pi, sin, cos
+# mengambil dan menampilkan image dari framework ShowBase. ShowBase juga digunakan unruk memberikan inputan dan gerakan
+from direct.showbase.ShowBase import ShowBase
+# manajemen kegiatan/fungsi pada python (event handling)
+from direct.task import Task
+# meload kelas aktor yang digunakan
+from direct.actor.Actor import Actor
+# memanipulasi waktu/durasi movement pada suatu nilai tertentu
+from direct.interval.IntervalGlobal import Sequence
+# mengatur koordinat aktor
+from panda3d.core import Point3
+from panda3d.core import ClockObject
+
+# direktori yang mengarahkan kunci untuk keatas,kebawah,kekiri,kekanan,dan putarbalik
+# secara default key di set pada kondisi False
+keyMap = {
+    "up": False,
+    "down": False,
+    "left": False,
+    "right": False,
+    "rotate": False
+}
+
+# untuk memanggil kembali fungsi untuk memperbarui peta kunci
+
+
+def updateKeyMap(key, state):
+    keyMap[key] = state
+
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)  # menginisialisasi modul ShowBase
+
+        # Nonaktifkan kontrol trackball kamera.
+        self.disableMouse()
+        # load image sesuai dengan direktori penyimpanan file
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.reparentTo(self.render)  # merender image yang telah di load
+        self.scene.setScale(0.25, 0.25, 0.25)  # mengatur skala image di layar
+        self.scene.setPos(-8, 42, 0)  # mengatur posisi image di layar
+
+        # mengatur pergerakan dan posisi kamera berdasar fungsi yang dibuat
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        # load image yang dijadikan aktor.
+        # image yang digunakan untuk pergerakan yaitu image ketika aktor diam dan image ketika aktor bergerak
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+
+        # melakukan iterasi image fungsi pergerakan
+        self.pandaActor.loop("walk")
+
+        # Membuat event sebagai arah yang akan ditekan melalui keyboard
+        self.accept("arrow_left", updateKeyMap, ["left", True])
+        self.accept("arrow_left-up", updateKeyMap, ["left", False])
+
+        self.accept("arrow_right", updateKeyMap, ["right", True])
+        self.accept("arrow_right-up", updateKeyMap, ["right", False])
+
+        self.accept("arrow_up", updateKeyMap, ["up", True])
+        self.accept("arrow_up-up", updateKeyMap, ["up", False])
+
+        self.accept("arrow_down", updateKeyMap, ["down", True])
+        self.accept("arrow_down-up", updateKeyMap, ["down", False])
+
+        self.accept("space", updateKeyMap, ["rotate", True])
+        self.accept("space-up", updateKeyMap, ["rotate", False])
+
+        # Memberi nilai Kecepatan serta arah
+        self.speed = 6
+        self.angle = 0  # secara default bernilai 0
+
+        self.taskMgr.add(self.update, "update")
+        # mengatur musik yang digunakan berdasarkan fungsi yang dibuat
+        self.loadMusic()
+
+    def spinCameraTask(self, task):
+        # task.time untuk mengembalikan nilai(float) yang menunjukkan berapa lama fungsi tugas ini berjalan sejak pertama dieksekusi.
+        # timer berjalan bahkan saat fungsi tidak dijalankan
+        # angleDegrees untuk mencari sudut kamera
+        angleDegrees = task.time * 6.0
+        # angleRadians untuk mendapatkan nilai radian dari sudut kamera tsb.
+        angleRadians = angleDegrees * (pi/180.0)
+        # setPos merupakan fungsi awal pergerakan kamera dimulai
+        # setHpr mengembalikan kamera ke koordinat semula
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+    def update(self, task):
+
+        globalClock = ClockObject.getGlobalClock()
+
+        dt = globalClock.getDt()
+
+        pos = self.pandaActor.getPos()
+
+        # Membuat beberapa pengaturan action sesuai keyMap yang sudah didaftarkan mulai dari posisinya speed nya dan clock timenya
+        if keyMap["left"]:
+            pos.x -= self.speed * dt
+        if keyMap["right"]:
+            pos.x += self.speed * dt
+        if keyMap["up"]:
+            pos.z += self.speed * dt
+        if keyMap["down"]:
+            pos.z -= self.speed * dt
+        if keyMap["rotate"]:
+            self.angle += 1  # akan mengubah nilai angle pada baris 64 karena objek akan berputar
+            self.pandaActor.setH(self.angle)
+
+        self.pandaActor.setPos(pos)
+
+        return task.cont
+
+    def loadMusic(self):
+        # load music/sound sesuai direktori penyimpanan file tsb
+        self.music = self.loader.loadMusic(
+            "models/audio/music-bg.wav")
+        self.music.setLoop(True)  # iterasi sound selama program dijalankan
+        self.music.play()  # musik menyala
+        self.music.setVolume(1)  # mengatur volume
+
+
+app = MyApp()
+app.run()
